65e29f19a4bf7e04772cecee5790fc033fbe090b
ymink716 <ymink716@gmail.com>
2020-06-30 19:50:01 +0900

    feat: userController에서 사용자가 신청, 등록한 매치를 보여주는 api 추가

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,6 +1,6 @@
 import { BaseController } from './BaseController';
 import { JsonController, Get, Put, UseBefore, HeaderParam, Req, BodyParam } from 'routing-controllers';
-import { PrismaClient, User } from '@prisma/client';
+import { PrismaClient, User, Level } from '@prisma/client';
 import { authMiddleware } from '../middlewares/auth';
 
 @JsonController('/users')
@@ -12,7 +12,8 @@ export class UserController extends BaseController {
         this.prisma = new PrismaClient();
     }
 
-    @Get()  // 유저 정보 조회
+    // 유저 정보 조회
+    @Get()  
     @UseBefore(authMiddleware)
     public getUser(@HeaderParam('authorization') token: string, @Req() req: any) {
         return {
@@ -20,26 +21,45 @@ export class UserController extends BaseController {
             user: req.user,
         }
     }
-
-    
     
     // 내가 등록한 매치 보기
+    @Get("/matches")
+    @UseBefore(authMiddleware)
+    public getMatchList(@HeaderParam('authorization') token: string, @Req() req: any) {
+        const matchList = this.prisma.match.findMany({
+            where: { writerId: req.user.id }
+        });
+
+        return { success: true, matchList }
+    }
 
     // 내가 신청한 매치 보기 (팀, 유저)
+    @Get("/applications")
+    @UseBefore(authMiddleware)
+    public getApplicationList(@HeaderParam('authorization') token: string, @Req() req: any) {
+        const teamApplicationList = this.prisma.matchTeamApplication.findMany({
+            where: { teamId: req.user.teamId }
+        });
+
+        const userApplicationList = this.prisma.matchUserApplication.findMany({
+            where: { userId: req.user.id }
+        });
+
+        return { success: true, teamApplicationList, userApplicationList }
+    }
 
     @Put()  // 내 정보 수정
     @UseBefore(authMiddleware)
-    public async updateUser(@HeaderParam('authorization') token: string, 
-                            @Req() req: any, 
-                      @BodyParam('name') name: string, 
-                      @BodyParam('age') age: number, 
-                      @BodyParam('phone') phone: string) {
+    public async updateUser(@HeaderParam('authorization') token: string, @Req() req: any, 
+                      @BodyParam('name') name: string, @BodyParam('age') age: number, 
+                      @BodyParam('phone') phone: string, @BodyParam('level') level: Level,
+                      @BodyParam('description') description: string) {
         
        try {
             const user: User = req.user;
             const updatedUser: User = await this.prisma.user.update({
                 where: { id: user.id },
-                data: { name, age, phone },
+                data: { name, age, phone, level, description },
             });
 
             return { success: true, updatedUser }
