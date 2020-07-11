9aaaec05a40976553dbb360ec3c1cada321dde90
RYZ <ryz@RYZui-MacBookAir.local>
2020-07-11 16:13:38 +0900

    add Sentry in app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,18 @@
 import 'reflect-metadata';
-import express from 'express';
+import express, { Request } from 'express';
 import { useExpressServer } from 'routing-controllers';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import morgan from 'morgan';
 import dotenv from 'dotenv';
+import * as Sentry from '@sentry/node';
 
+Sentry.init({ dsn: process.env.SENTRY_DSN });
 const app = express();
 
 dotenv.config();
+
+app.use(Sentry.Handlers.requestHandler());
 
 // 테스트 후 삭제 예정
 app.engine('html', require('ejs').renderFile);
@@ -25,6 +29,15 @@ useExpressServer(app, {
   controllers: [`${__dirname}/controllers/**`],
   middlewares: [`${__dirname}/middlewares/**`],
   });
+
+app.use(Sentry.Handlers.errorHandler());
+
+app.use(function onError(_: any , __: Request, res: any, next: any) {
+  // The error id is attached to `res.sentry` to be returned
+  // and optionally displayed to the user for support.
+  res.statusCode = 500;
+  res.end(res.sentry + "\n");
+});
 
 
 export {
