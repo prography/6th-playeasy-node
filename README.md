70617d996d7fd6cc755078a9414a731144c97801
ymink716 <ymink716@gmail.com>
2020-09-13 19:19:06 +0900

    프로젝트 세팅

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# 6th-playeasy-node
-플레이지 백엔드
+# Awesome Project Build with TypeORM
+
+Steps to run this project:
+
+1. Run `npm i` command
+2. Setup database settings inside `ormconfig.json` file
+3. Run `npm start` command
--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,22 @@
 import 'reflect-metadata';
-import express, { Request } from 'express';
+import express from 'express';
+import { createConnection } from 'typeorm';
 import { useExpressServer } from 'routing-controllers';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import morgan from 'morgan';
 import dotenv from 'dotenv';
-import createError from 'http-errors';
-import * as Sentry from '@sentry/node';
 
-Sentry.init({ dsn: process.env.SENTRY_DSN });
+createConnection()
+  .then(() => {
+    console.log('database connected');
+  })
+  .catch((error) => console.error(error));
+  
 const app = express();
 
 dotenv.config();
 
-app.use(Sentry.Handlers.requestHandler());
-
-// 테스트 후 삭제 예정
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'ejs');
 
@@ -29,20 +30,9 @@ useExpressServer(app, {
   cors: true,
   controllers: [`${__dirname}/controllers/**`],
   middlewares: [`${__dirname}/middlewares/**`],
-});
-
-app.use(Sentry.Handlers.errorHandler());
-
-app.use(function onError(_: any , __: Request, res: any, next: any) {
-  // The error id is attached to `res.sentry` to be returned
-  // and optionally displayed to the user for support.
-  res.statusCode = 500;
-  res.end(res.sentry + "\n");
-});
-
+  });
 
 
 export {
     app,
 }
-
