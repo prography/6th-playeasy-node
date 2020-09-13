f9e24b9bf2c55783eafa2fd1ac2810765cf0a0bb
ymink716 <ymink716@gmail.com>
2020-09-13 22:35:06 +0900

    refactor: UserController

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -1,16 +1,24 @@
 import { BaseController } from './BaseController';
-import { JsonController, Get, Put, UseBefore, Req, BodyParam, QueryParam, Body } from 'routing-controllers';
-import { PrismaClient, User, Level, Team } from '@prisma/client';
+import { 
+    JsonController, 
+    Get, 
+    Put, 
+    UseBefore, 
+    Req, 
+    BodyParam, 
+} from 'routing-controllers';
 import { isLoggedIn } from '../middlewares/auth';
+import { UserService } from '../service/UserService';
+import { MatchService } from '../service/MatchService';
+import { MatchApplicationService } from '../service/MatchApplicationService';
 
 @JsonController('/users')
 export class UserController extends BaseController {
-    private prisma: PrismaClient;
-    
-    constructor() {
-        super();
-        this.prisma = new PrismaClient();
-    }
+    // 생성자 주입을 사용한 의존성 주입
+    constructor(
+        private userService: UserService,
+        private matchService: MatchService,
+        private matchAppicationService: MatchApplicationService) { super(); }
 
     // 내 정보
     @Get()  
@@ -26,125 +34,33 @@ export class UserController extends BaseController {
     @Get("/matches")
     @UseBefore(isLoggedIn)
     public async getMatchList(@Req() req: any)  {
-        try {
-            const matchList = await this.prisma.match.findMany({
-                where: { writerId: req.user.id },
-                select: {
-                    id: true, type: true, description: true,
-                    startAt: true, duration: true, fee: true,
-                    phone: true, totalQuota: true, status: true, 
-                    writerId: true,
-                    homeTeam: {
-                        select: {
-                            id: true, name: true, description: true,
-                            age: true, level: true, leader: true, phone: true
-                        }
-                    },
-                    location: {
-                        select: {
-                            id: true, latitude: true, longitude: true,
-                            name: true, address: true, detail: true,
-                        }
-                    },
-                }
-            });
+        // 매치서비스에서 내가 등록한 매치를 가져온다
+        const userId = req.user.id;
+        const matches = await this.matchService.getMatchByWriter(userId);
 
-            return { matchList } 
-        } catch (error) {
-            throw error;
-        }
+        return matches;
     }
 
     // 나의 매치 정보 - 나의 신청 현황
     @Get("/applications")
     @UseBefore(isLoggedIn)
-    public async getApplicationList(@Req() req: any, @QueryParam("type") type: string) {
-        try {
-            let applicationList;
-            if (type === "team") {
-                applicationList = await this.prisma.matchTeamApplication.findMany({
-                    where: { teamId: req.user.teamId },
-                    select: {
-                        id: true, quota: true, status: true,
-                        match: {
-                            select: {
-                                id: true, startAt: true, duration: true,
-                                location: {
-                                    select: {
-                                        id: true,
-                                        name: true,
-                                        address: true,
-                                        detail: true,
-                                    }
-                                }
-                            }
-                        },
-                    }
-                });                
-            } else if (type === "personal") {
-                applicationList = await this.prisma.matchUserApplication.findMany({
-                    where: { userId: req.user.id },
-                    select: {
-                        id: true, quota: true, status: true,
-                        match: {
-                            select: {
-                                id: true, startAt: true, duration: true,
-                                location: {
-                                    select: {
-                                        id: true,
-                                        name: true,
-                                        address: true,
-                                        detail: true,
-                                    }
-                                }
-                            }
-                        },
-                    }
-                });         
-            } else {
-                throw new Error('올바른 Param 값이 전달되지 않았습니다.');
-            }
+    public async getApplicationList(@Req() req: any) {
+        // 매치신청 서비스에서 사용자가 신청한 매치신청목록을 가져온다
+        const userId = req.user.id;
+        const applications = await this.matchAppicationService.getMatchApplicationsByUser(userId);
 
-            return { applicationList }
-        } catch (error) {
-            throw error;
-        }
+        return applications;
     }
 
     // 내 정보 수정 
     @Put()  
     @UseBefore(isLoggedIn)
     public async updateUser(@Req() req: any, 
-                            @BodyParam('userData') userData: User) {   
-        try {
-            const exUser: User = req.user;
+                            @BodyParam('userData') updateUserDto: UpdateUserDto) {   
+        // 유저 서비스에서 내 정보를 업데이트한다
+        const userId = req.user.id;
+        const user = await this.userService.updateUser(userId);
 
-            const name: string | null = userData.name;
-            const age: number | null = userData.age;
-            const phone: string | null = userData.phone;
-            const level: Level | null = userData.level;
-            const description: string | null = userData.description;
-            
-            const user: object = await this.prisma.user.update({
-                where: { id: exUser.id },
-                data: { name, age, phone, level, description },
-                select: {
-                    id: true, name: true, age: true, email: true,
-                    socialType: true, phone: true, pushToken: true,
-                    level: true, description: true, picture: true,
-                    team: {
-                        select: {
-                            id: true, name: true, description: true,
-                            age: true, level: true, leader: true, phone: true,
-                        }
-                    }
-                }
-            });
-
-            return { user }
-        } catch (error) {
-            throw error;
-        }
+        return user;
     }
-    // 사진 
 }
