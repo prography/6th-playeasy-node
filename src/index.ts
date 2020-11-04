4445ffbdfd42180acc15c1a859487433f6feb20b
ymink716 <ymink716@gmail.com>
2020-11-04 18:35:01 +0900

    fix: index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,12 @@ import { app } from './app';
 
 dotenv.config();
 
-const HOST = process.env.HOST || 'localhost';
+//const HOST = process.env.HOST || 'localhost';
 const PORT = Number(process.env.PORT || 3000);
 
 const startApplication = async () => {
-  app.listen(PORT, HOST, () => {
-    console.log(`server is running on ${HOST}:${PORT}`);
+  app.listen(PORT, () => {
+    console.log(`server is running on ${PORT} port`);
   });
 };
 
